bab7a4dc1873ae3e73a8012b819998cd76b29e5e
manmarru <soboru123@gmail.com>
2025-08-25 15:18:50 +0900

    컵 분배

--- CupTower_Server/Board.cs
+++ CupTower_Server/Board.cs
@@ -1,47 +1,41 @@
 
 struct USER
 {
-    public int Remain0;
-    public int Remain1;
-    public int Remain2;
+    public USER() { }
+
+    public int[] Remain = new int[3]{0, 0, 0};
 }
 
 class CBoard
 {
     private List<USER> UserCups;
     private readonly int m_MaxUser;
-    
+
     public CBoard(int MaxUser)
     {
         m_MaxUser = MaxUser;
-        UserCups = new List<USER>();
+        USER[] Users = new USER[MaxUser];
+        List<int> Cups = new List<int>();
 
         Random rand = new Random();
 
-        int Remain0 = 9;
-        int Remain1 = 9;
-        int Remain2 = 9;
-
-        for (int user = 0; user < MaxUser; ++user)
+        for (int i = 0; i < 9; ++i)
         {
-            int Sum = 0;
-            USER UserCup = new USER();
-            UserCup.Remain0 = rand.Next(0, Math.Min(Remain0, 9 - Sum));
-            Remain0 -= UserCup.Remain0;
-            Sum += UserCup.Remain0;
-
-            UserCup.Remain1 = rand.Next(0, Math.Min(Remain1, 9 - Sum));
-            Remain1 -= UserCup.Remain1;
-            Sum += UserCup.Remain1;
-
-            UserCup.Remain2 = 9 - Sum;
-            Remain2 -= Sum;
-
-            UserCups.Add(UserCup);
+            Cups.Add(0);
+            Cups.Add(1);
+            Cups.Add(2);
         }
-        
 
-
+        int Index;
+        for (int player = 0; player < MaxUser; ++player)
+        {
+            for (int i = 1; i <= 9; ++i)
+            {
+                Index = rand.Next(Cups.Count);
+                ++Users[player].Remain[Cups[Index]];
+                Cups.RemoveAt(Index);
+            }
+        }
     }
 }
 
